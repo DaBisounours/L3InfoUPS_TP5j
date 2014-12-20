7b3f28217bf9929e4d7bdd380846254ebe7ece2c
DaBisounours <evan.graine@gmail.com>
2014-12-20 23:08:34 +0100

    Adding a Class
    - Finished ExprEmptyList
    - Starting ExprAddElemList to evaluate an addition of an element into a list (Caml : @)

--- src/expr/ExprEmptyList.java
+++ src/expr/ExprEmptyList.java
@@ -1,7 +1,34 @@
 package expr;
 
+import type.TList;
+import type.Type;
+import valeur.Valeur;
+import valeur.ValeurList;
+
+import java.util.Map;
+
 /**
- * Created by bisounours on 12/20/14.
+ * Created by Evan Graïne on 12/20/14.
  */
-public class ExprEmptyList {
+public class ExprEmptyList implements Expr {
+
+    @Override
+    public Valeur evaluate(String expression, Map<String, Valeur> env) throws ExprEvaluationException {
+        expression.replaceAll("\\s", "");
+        if(expression.contentEquals("[]"));
+        return new ValeurList<Object>(null);
+    }
+
+    @Override
+    public Type type(String varName, Map<String, Type> env) throws ExprEvaluationException {
+        if(env!=null) {
+            if (env.get(varName) != null) return env.get(varName);
+        }
+        else
+        {
+            if(varName.contentEquals("[]"));
+                return new TList(null);
+        }
+        throw new ExprEvaluationException("Wrong type for expression "+varName+"\nExpected empty list");
+    }
 }
